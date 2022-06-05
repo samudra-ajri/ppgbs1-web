2164d544b1f957b51a626b3563d91090de310d11
samudra-ajri <54126488+samudra-ajri@users.noreply.github.com>
2022-06-05 21:18:31 +0700

    Add target card

---
+++ src/components/TargetCard.jsx
@@ -0,0 +1,15 @@
+import { Card, CardContent, Typography } from '@mui/material'
+
+function TargetCard() {
+  return (
+    <Card sx={{ mb: 1 }}>
+      <CardContent>
+        <Typography variant="h7" component="div">
+          Alquran
+        </Typography>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default TargetCard
