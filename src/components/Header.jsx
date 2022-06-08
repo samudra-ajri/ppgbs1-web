62939175945846b85bcf23bf79fb7a40b448b6c8
samudra-ajri <54126488+samudra-ajri@users.noreply.github.com>
2022-06-08 22:02:36 +0700

    Remove Pigaru logo sample

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import { AppBar, Toolbar, Typography } from '@mui/material'
 import { Box } from '@mui/system'
-import AdbIcon from '@mui/icons-material/Adb'
 
 function Header() {
   return (
@@ -8,7 +7,6 @@ function Header() {
       <Box sx={{ pt: 10 }}>
         <AppBar color='default'>
           <Toolbar>
-            <AdbIcon />
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               P I G A R U
             </Typography>
