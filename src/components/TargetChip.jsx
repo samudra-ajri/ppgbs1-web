04012f51bf68b3529831b9938ae54320408e38b6
samudra-ajri <54126488+samudra-ajri@users.noreply.github.com>
2022-06-19 11:06:03 +0700

    Add onclick chips

--- src/components/TargetChip.jsx
+++ src/components/TargetChip.jsx
@@ -2,13 +2,15 @@ import { Box, Chip } from '@mui/material'
 
 function TargetChip(props) {
   const targets = props.subject.targets
+
   const style = () => {
     if (
       props.subject.category === 'ALQURAN' ||
       props.subject.category === 'HADITS'
     ) return { m: 0.5, width: '15%' }
-    return { m: 0.5}
+    return { m: 0.5 }
   }
+
   const align = () => {
     if (
       props.subject.category === 'ALQURAN' ||
@@ -16,12 +18,15 @@ function TargetChip(props) {
     ) return 'center'
     return 'left'
   }
+
+  const handleClick = () => {}
+
   return (
     <>
       <Box textAlign={align()}>
         {targets.map((target) => (
           <Chip variant='outlined' key={target} sx={style()}
-            label={target} color='success' />
+            label={target} color='success' onClick={handleClick}/>
         ))}
       </Box>
     </>
