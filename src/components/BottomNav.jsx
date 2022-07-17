b541a7765b5a44bc62a1d0f3a9a96702cd35a267
samudra-ajri <54126488+samudra-ajri@users.noreply.github.com>
2022-07-17 14:11:52 +0700

    Add admin buttons nav

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,51 +1,45 @@
 import { BottomNavigation, BottomNavigationAction, Paper } from "@mui/material"
-import HomeIcon from '@mui/icons-material/HomeRounded'
 import LoginIcon from '@mui/icons-material/LoginRounded'
 import PersonIcon from '@mui/icons-material/PersonRounded'
-import LogoutIcon from '@mui/icons-material/LogoutRounded'
 import ProfileIcon from '@mui/icons-material/FaceOutlined'
+import BarIcon from '@mui/icons-material/EqualizerRounded';
+import DashboardIcon from '@mui/icons-material/DataUsageRounded';
+import PeopleListIcon from '@mui/icons-material/RecentActorsOutlined';
 import { useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 import { Box } from "@mui/system"
-import { useDispatch, useSelector } from "react-redux"
-import { logout, reset } from "../features/auth/authSlice"
+import { useSelector } from "react-redux"
 
 function BottomNav() {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
   const { user } = useSelector((state) => state.auth)
   const [value, setValue] = useState(0)
-  
-  const onLogout = () => {
-    dispatch(logout())
-    dispatch(reset())
-    navigate('/')
-  }
 
   return (
     <Box sx={{ pb: 10 }}>
-    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        {user ? (
-          [
-            <BottomNavigationAction key="home" component={Link} to="/" label="Beranda" icon={<HomeIcon />} />,
-            <BottomNavigationAction key="profile" component={Link} to="/profile" label="Akun Saya" icon={<ProfileIcon />} />,
-            <BottomNavigationAction key="logout" onClick={onLogout} label="Keluar" icon={<LogoutIcon />} />
-          ]
-        ) : (
-          [
-            <BottomNavigationAction key="login" component={Link} to="/login" label="Masuk" icon={<LoginIcon />} />,
-            <BottomNavigationAction key="register" component={Link} to="/register" label="Registrasi" icon={<PersonIcon />} />
-          ]
-        )}
-      </BottomNavigation>
-    </Paper>
+      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          {user ? (
+              user.role !== 'GENERUS' ? [
+                <BottomNavigationAction key="dashboard" component={Link} to="/" label="Dashboard" value="dashboard" icon={<DashboardIcon />} />,
+                <BottomNavigationAction key="progress" component={Link} to="/" label="Materi" value="progress" icon={<BarIcon />} />,
+                <BottomNavigationAction key="people" component={Link} to="/" label="Generus" value="people" icon={<PeopleListIcon />} />,
+              ] : [
+                <BottomNavigationAction key="profile" component={Link} to="/profile" label="Akun Saya" value="profile" icon={<ProfileIcon />} />,
+              ]
+          ) : (
+            [
+              <BottomNavigationAction key="login" component={Link} to="/login" label="Masuk" value="login" icon={<LoginIcon />} />,
+              <BottomNavigationAction key="register" component={Link} to="/register" label="Registrasi" value="register" icon={<PersonIcon />} />
+            ]
+          )}
+        </BottomNavigation>
+      </Paper>
     </Box>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
-import { AppBar, Toolbar, Typography } from '@mui/material'
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material'
 import { Box } from '@mui/system'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import LogoutIcon from '@mui/icons-material/LogoutRounded'
+import { logout, reset } from "../features/auth/authSlice"
 
 function Header() {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const { user } = useSelector((state) => state.auth)
+
+  const onLogout = () => {
+    dispatch(logout())
+    dispatch(reset())
+    navigate('/')
+  }
+
   return (
     <>
       <Box sx={{ pt: 10 }}>
@@ -10,6 +24,7 @@ function Header() {
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               P I G A R U
             </Typography>
+            {user ? (<IconButton onClick={onLogout}><LogoutIcon /></IconButton>) : (<></>)}
           </Toolbar>
         </AppBar>
       </Box>
