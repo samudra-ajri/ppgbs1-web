e5b14f9449b2fbe437ab0e59bf8db900c7904111
samudra-ajri <54126488+samudra-ajri@users.noreply.github.com>
2022-07-19 23:37:46 +0700

    Modify user card enhanced

--- src/components/PeopleCard.jsx
+++ src/components/PeopleCard.jsx
@@ -1,27 +1,48 @@
-import { Card, CardActionArea, CardContent, Grid, Typography } from '@mui/material'
-import { Link } from 'react-router-dom'
+import { Card, CardContent, Grid, IconButton, Tooltip, Typography } from '@mui/material'
+import DeleteIcon from '@mui/icons-material/DeleteOutlineRounded'
+import CircleIcon from '@mui/icons-material/FiberManualRecordRounded'
 
-function PeopleCard(props) {
+function PeopleCard() {
+  //F9CEEE CCF3EE
+
+  const onClick = () => {
+    console.log(123);
+  }
+
   return (
-    <Card variant="outlined" sx={{ mb: 0.5 }}>
-      <Link to={`/`} component={CardActionArea}>
-        <CardContent sx={{padding: 2,
-    "&:last-child": {
-      paddingBottom: 2
-    }}}>
-          <Grid container>
-            <Grid item xs={9}>
-              <Typography variant="body1">Samudra Ajri Kifli</Typography>
+    <Card sx={{ mb: 0.5, cursor: 'pointer' }}>
+      <CardContent sx={{
+        padding: 2,
+        '&:last-child': {
+          paddingBottom: 2
+        }
+      }}>
+        <Grid container>
+          <Grid item xs={10} onClick={onClick}>
+            <Grid container>
+              <Grid item>
+              <Typography variant='body1'>
+                Samudra Ajri Kifli
+              </Typography>
+              </Grid>
+              <Grid item>
+              <Tooltip title='perempuan'>
+                <CircleIcon fontSize='small' sx={{ color: '#F9CEEE' }} />
+              </Tooltip>
+              </Grid>
+
             </Grid>
-            <Grid item xs={2}>
-              <Typography align='left' variant="body2">75%</Typography>
-            </Grid>
-            <Grid item>
-              <Typography align='right' variant="body2">X</Typography>
-            </Grid>
+            <Typography variant='caption' color='text.secondary'>Margahayu - Margahayu Utara</Typography>
           </Grid>
-        </CardContent>
-      </Link>
+          <Grid item>
+            <Tooltip title='hapus user'>
+              <IconButton align='right' onClick={onClick}>
+                <DeleteIcon fontSize='medium' color='error' />
+              </IconButton>
+            </Tooltip>
+          </Grid>
+        </Grid>
+      </CardContent>
     </Card>
   )
 }
