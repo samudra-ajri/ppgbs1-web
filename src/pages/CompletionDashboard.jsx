3beb19dfb134df6ecc27f8791d2653bb8dec327e
samudra-ajri <54126488+samudra-ajri@users.noreply.github.com>
2022-07-28 15:11:10 +0700

    Add completions dashboard page

--- src/pages/CompletionDashboard.jsx
+++ src/pages/CompletionDashboard.jsx
@@ -1,9 +1,17 @@
-import { Typography } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
+import CircularProgressWithLabel from '../components/CircularProgressWithLabel'
 
 function CompletionDashboard() {
   return (
     <>
       <Typography variant='h6' align='center' sx={{ mb: 1 }}>Capaian Materi Generus</Typography>
+      <Grid container align='center' spacing={1}>
+      <CircularProgressWithLabel value={90} title='Total'/>
+      <CircularProgressWithLabel value={30} title='Alquran'/>
+      <CircularProgressWithLabel value={65} title='Alhadits'/>
+      <CircularProgressWithLabel value={65} title='Penunjang'/>
+      <CircularProgressWithLabel value={65} title='Hafalan'/>
+      </Grid>
     </>
   )
 }
