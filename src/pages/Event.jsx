7b9b70c2a4f64e04ebc7727e7ab6a8005ec8b779
samudra-ajri <54126488+samudra-ajri@users.noreply.github.com>
2022-09-25 11:34:51 +0700

    Add 'belum ada kegiatan' state

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -30,6 +30,7 @@ function Event() {
       <Typography variant='h6' align='center' sx={{ mb: 1 }}>Jadwal Kegiatan</Typography>
       {(user.role !== 'GENERUS' && user.role !=='MT' && user.role !=='MS') && <Button size="medium" style={{ margin: "20px auto" }} type="submit" variant="contained" color="info" fullWidth onClick={onClick}>Tambah</Button>}
       {isSuccess && events.events?.map(event => <EventCard key={event._id} event={event} user={user}/>)}
+      {isSuccess && events.events?.length === 0 && <Typography textAlign='center' pt={3} color='text.secondary' variant='body2'>Belum ada kegiatan.</Typography>}
     </>
   )
 }
