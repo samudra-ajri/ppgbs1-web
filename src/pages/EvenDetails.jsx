40d813c33e455abfd1fd8f71063ebd446ca72945
samudra-ajri <54126488+samudra-ajri@users.noreply.github.com>
2022-10-04 20:56:35 +0700

    Modify pathname using uselocation

--- src/pages/EvenDetails.jsx
+++ src/pages/EvenDetails.jsx
@@ -1,13 +1,15 @@
 import { Typography } from '@mui/material'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import BackHeader from '../components/BackHeader'
 import PresenceList from '../components/PresenceList'
 import { getEvent, reset } from '../features/event/eventSlice'
 
 function EventPresence() {
-  const eventId = window.location.pathname.split('/')[3]
+  const location = useLocation()
+  const eventId = location.pathname.split('/')[3]
+
   const dispatch = useDispatch()
   const navigate = useNavigate()
   const { user } = useSelector((state) => state.auth)
