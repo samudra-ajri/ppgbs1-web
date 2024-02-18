020aa87b075610760ed057af29d1e18b3a78bb6b
Samudra Ajri Kifli <samudra1ajri@gmail.com>
2024-02-18 13:55:12 +0700

    feat(person-completion): last updated

--- src/pages/PersonCompletion.jsx
+++ src/pages/PersonCompletion.jsx
@@ -2,13 +2,20 @@ import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux"
 import ProfileCard from "../components/ProfileCard"
-import { Card, CardContent, CircularProgress, Grid } from "@mui/material"
+import {
+  Card,
+  CardContent,
+  CircularProgress,
+  Grid,
+  Typography,
+} from "@mui/material"
 import {
   getSumCompletions,
   reset,
 } from "../features/completionScores/completionScoreSlice"
 import SumCompletionCard from "../components/SumCompletionCard"
 import { logout } from "../features/auth/authSlice"
+import moment from "moment"
 
 function UserCompletion() {
   const dispatch = useDispatch()
@@ -28,6 +35,17 @@ function UserCompletion() {
     dispatch(reset())
   }, [person, isError, navigate, dispatch, message])
 
+  const lastUpdateTime = () => {
+    const lastCompletionUpdate = person?.lastCompletionUpdate
+    if (lastCompletionUpdate) {
+      const startDateObject = moment(new Date(Number(lastCompletionUpdate)))
+      const startDate = startDateObject.format("DD MMM YYYY")
+      const startTime = startDateObject.format("HH.mm")
+      return `${startDate} pkl. ${startTime}`
+    }
+    return "-"
+  }
+
   return (
     <>
       <ProfileCard user={person} />
@@ -40,18 +58,32 @@ function UserCompletion() {
           </Card>
         </Grid>
       ) : (
-        <Grid container pb={10} spacing={2}>
-          {sumCompletions.map((sumCompletion, index) => (
-            <Grid item xs={6} key={index}>
-              <SumCompletionCard
-                key={index}
-                percentage={sumCompletion.percentage}
-                title={sumCompletion.category}
-                link={`/c/person-completion/${sumCompletion.category}`}
-              />
+        <>
+          <Grid container pt={2} pb={2}>
+            <Grid item>
+              <Typography
+                align='center'
+                color='text.secondary'
+                fontSize={12}
+                variant='subtitle2'
+              >
+                Update terakhir pada: {lastUpdateTime()}
+              </Typography>
             </Grid>
-          ))}
-        </Grid>
+          </Grid>
+          <Grid container pb={10} spacing={2}>
+            {sumCompletions.map((sumCompletion, index) => (
+              <Grid item xs={6} key={index}>
+                <SumCompletionCard
+                  key={index}
+                  percentage={sumCompletion.percentage}
+                  title={sumCompletion.category}
+                  link={`/c/person-completion/${sumCompletion.category}`}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </>
       )}
     </>
   )
