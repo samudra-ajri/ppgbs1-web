a553a27c20cc52f510868f20f6614defe95dc339
Samudra Ajri Kifli <samudra1ajri@gmail.com>
2024-05-01 15:15:55 +0700

    feat(footer): feedback button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,53 @@
-import { Paper, Typography } from "@mui/material"
+import styled from "@emotion/styled"
+import { Button, Grid, Typography } from "@mui/material"
+import ChatIcon from "@mui/icons-material/Chat" // Import the chat icon
+
+const FeedbackButton = styled(Button)({
+  backgroundColor: "#eff2ef", // Adjust the color to match the button background color in your image
+  color: "#76859B",
+  padding: "5px 10px",
+  textTransform: "none",
+  fontSize: 12, // Set your desired font size
+  fontWeight: "none",
+  boxShadow: "none",
+  borderRadius: "100px", // Adds rounded corners. Adjust this value to increase or decrease the roundness
+  "&:hover": {
+    backgroundColor: "#eff2ef", // Darker shade for hover effect
+    boxShadow: "none",
+  },
+})
+
+const openInNewTab = (url) => {
+  window.open(url, "_blank", "noreferrer")
+}
 
 function Footer() {
   return (
-    <Paper sx={{ position: 'static', bottom: 80, left: 0, right: 0, padding: 3 }} elevation={0}>
-      <Typography 
-        align="center" 
-        sx={{ color:'#76859B', fontSize: 10 }}
-        pt={10}
-        pb={10}
-      >
-        <b translate="no">PIGARU</b> | Pusat Informasi Generus Bandung Selatan I
-      </Typography>
-      </Paper>
+    <Grid
+      container
+      spacing={2}
+      paddingTop={10}
+      paddingBottom={10}
+      textAlign='center'
+    >
+      <Grid item xs={12}>
+        <Typography sx={{ color: "#76859B", fontSize: 12 }}>
+          <b translate='no'>PIGARU</b> | Pusat Informasi Generus Bandung Selatan
+          I
+        </Typography>
+      </Grid>
+
+      <Grid item xs={12}>
+        <FeedbackButton
+          variant='contained'
+          startIcon={<ChatIcon />}
+          size='small'
+          onClick={() => openInNewTab("https://forms.gle/aKzq8NPP3nEeB8wQ6")}
+        >
+          Saran & Pertanyaan
+        </FeedbackButton>
+      </Grid>
+    </Grid>
   )
 }
 
